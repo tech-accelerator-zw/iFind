bd751bbe3642647aad0610f1594f3280ff968f4d
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-07-07 06:51:42 +0200

    Initial Commit

---
+++ src/main/java/com/techaccelarators/ifind/IFindApplication.java
@@ -0,0 +1,13 @@
+package com.techaccelarators.ifind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IFindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techaccelarators/ifind/IFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techaccelarators.ifind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IFindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
