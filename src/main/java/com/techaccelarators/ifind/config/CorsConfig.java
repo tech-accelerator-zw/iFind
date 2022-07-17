21529fa5bd8b3de88e7103f155490f60ed0f51fa
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-07-17 14:09:25 +0200

    Added Cors Configuration fix

--- src/main/java/com/techaccelarators/ifind/config/CorsConfig.java
+++ src/main/java/com/techaccelarators/ifind/config/CorsConfig.java
@@ -1,22 +1,22 @@
-package com.techaccelarators.ifind.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfiguration getCorsConfiguration(){
-        return new WebMvcConfiguration(){
-            @Override
-            public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
-}
+//package com.techaccelarators.ifind.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public WebMvcConfiguration getCorsConfiguration(){
+//        return new WebMvcConfiguration(){
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry){
+//                registry.addMapping("/**")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedOrigins("*")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+//}
