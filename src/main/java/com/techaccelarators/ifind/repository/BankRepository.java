cd3f7bbfcf2741434504d6190fa556f7c9435c04
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-07-21 12:59:10 +0200

    Bank Endpoints now working

--- src/main/java/com/techaccelarators/ifind/repository/BankRepository.java
+++ src/main/java/com/techaccelarators/ifind/repository/BankRepository.java
@@ -1,7 +1,18 @@
 package com.techaccelarators.ifind.repository;
 
 import com.techaccelarators.ifind.domain.Bank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BankRepository extends JpaRepository<Bank,Long> {
+    Optional<Bank> findByNameIgnoreCase(String name);
+
+    boolean existsByName(String name);
+
+    Optional<Bank> findBankByName(String name);
+
+    Page<Bank> findAllByNameLikeIgnoreCase(String searchWord, Pageable pageable);
 }
--- src/main/java/com/techaccelarators/ifind/service/impl/BankServiceImpl.java
+++ src/main/java/com/techaccelarators/ifind/service/impl/BankServiceImpl.java
@@ -1,53 +1,96 @@
 package com.techaccelarators.ifind.service.impl;
 
 import com.techaccelarators.ifind.domain.Bank;
+import com.techaccelarators.ifind.domain.enums.Status;
 import com.techaccelarators.ifind.dtos.bank.BankRequestDto;
+import com.techaccelarators.ifind.exception.InvalidRequestException;
+import com.techaccelarators.ifind.exception.RecordNotFoundException;
+import com.techaccelarators.ifind.repository.BankRepository;
 import com.techaccelarators.ifind.service.BankService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class BankServiceImpl implements BankService {
+
+    private final BankRepository bankRepository;
     @Override
     public Bank createBank(BankRequestDto bankRequestDto) {
-        return null;
+        checkUnique(bankRequestDto, null);
+        Bank bank = Bank.builder()
+                .name(bankRequestDto.getName())
+                .build();
+        bank.setStatus(Status.ACTIVE);
+        return bankRepository.save(bank);
     }
 
     @Override
     public Bank updateBank(Long id, BankRequestDto bankRequestDto) {
-        return null;
+        Bank bank = getBankById(id);
+        checkUnique(bankRequestDto, id);
+        bank.setName(bankRequestDto.getName());
+        if(bankRepository.existsByName(bankRequestDto.getName())){
+            throw new InvalidRequestException("Bank Name Already In Use");
+        }
+
+        return bankRepository.save(bank);
     }
 
     @Override
     public Page<Bank> getAllBanks(Pageable pageable) {
-        return null;
+        return bankRepository.findAll(pageable);
     }
 
     @Override
     public Bank getBankById(Long id) {
-        return null;
+        return bankRepository.findById(id)
+                .orElseThrow(()-> new RecordNotFoundException("Bank Not Found"));
     }
 
     @Override
     public Bank getBankByName(String name) {
-        return null;
+        return bankRepository.findBankByName(name)
+                .orElseThrow(()-> new RecordNotFoundException("Bank Not Found"));
     }
 
     @Override
     public Page<Bank> searchBank(String searchParam, Pageable pageable) {
-        return null;
+        try {
+            String searchWord = "%".concat(searchParam).concat("%");
+            return bankRepository.findAllByNameLikeIgnoreCase(searchWord, pageable);
+        } catch (Exception ex) {
+            throw new InvalidRequestException(ex.getMessage());
+        }
     }
 
     @Override
     public Bank toggleBankStatus(Long id) {
-        return null;
+        Bank bank = getBankById(id);
+
+        if (bank.getStatus() == Status.ACTIVE) {
+            bank.setStatus(Status.INACTIVE);
+        } else if (bank.getStatus() == Status.INACTIVE) {
+            bank.setStatus(Status.ACTIVE);
+        }
+        return bankRepository.save(bank);
     }
 
     @Override
     public void deleteBank(Long id) {
-
+        Bank bank = getBankById(id);
+        bankRepository.delete(bank);
+    }
+    private void checkUnique(BankRequestDto request, Long id) {
+        log.info("Checking bank uniqueness with name: {} and id: {}", request.getName(), id);
+        bankRepository.findByNameIgnoreCase(request.getName())
+                .filter(customer -> !customer.getId().equals(id))
+                .ifPresent(customer -> {
+                    throw new InvalidRequestException("Bank with given name already exists");
+                });
     }
 }
