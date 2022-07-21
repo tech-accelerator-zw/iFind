20dcdffeeb31d8ab636f23d8df741c996b133934
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-07-21 21:15:12 +0200

    Revertred back to cors httpSecurity

--- src/main/java/com/techaccelarators/ifind/config/SecurityConfig.java
+++ src/main/java/com/techaccelarators/ifind/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
