199c1a8ac5a2918ae26dc7d8217b0c79efbb718e
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-07-31 10:26:26 +0200

    mmcz cors configs with  tweks

--- src/main/java/com/techaccelarators/ifind/config/SecurityConfig.java
+++ src/main/java/com/techaccelarators/ifind/config/SecurityConfig.java
@@ -4,10 +4,8 @@ import com.techaccelarators.ifind.security.CustomUserDetailsService;
 import com.techaccelarators.ifind.security.JwtAuthenticationEntryPoint;
 import com.techaccelarators.ifind.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -20,8 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -78,18 +76,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-    @Bean
-    public FilterRegistrationBean corsFilterBean() {
+   @Bean
+    CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+//        configuration.setExposedHeaders(Arrays.asList("*"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        FilterRegistrationBean corsFilter = new FilterRegistrationBean(new CorsFilter(source));
-        corsFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return corsFilter;
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 
 }
